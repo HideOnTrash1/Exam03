18d81b27be3f609d9197acd9b3f90ebf18a294ab
m0_46317574 <1228247640@qq.com>
2022-05-25 14:56:15 +0800

    Initial commit

--- src/test/java/test.java
+++ src/test/java/test.java
@@ -1,10 +1,12 @@
 
 import cn.edu.hncj.demo.Calculation;
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class test {
-    public static void main(String[] args) {
+    @Test
+    public void demotest(){
         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
         Calculation calculation = context.getBean(Calculation.class);
         calculation.add();
